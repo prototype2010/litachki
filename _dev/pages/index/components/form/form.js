a263126460cae59a5a76512c67a9f76d05f3bdea
Alexander Z <o.zakorko@soft-industry.com>
2019-03-12 18:16:10 +0200

    form + notifications + stuff

--- _dev/pages/index/components/form/form.js
+++ _dev/pages/index/components/form/form.js
@@ -1,8 +1,31 @@
 import Inputmask from 'inputmask';
+import _ from 'lodash';
+import toastr from 'toastr';
+import '../../../../style/toastr-custom.css';
+import axios from 'axios';
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-top-full-width",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "6000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
 
 document.addEventListener('DOMContentLoaded', () => {
 
     const phoneSelect = document.getElementById('country');
+    const input = document.getElementById("phone");
 
     phoneSelect.addEventListener('change', ({target}) => {
         initInputMaskByLanguage(target.value)
@@ -17,7 +40,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 break;
             }
             case 'ru' : {
-                applyInputMask(new Inputmask("+7 (\\99) 999-99-99"));
+                applyInputMask(new Inputmask("+7 (\\999) 999-99-99"));
                 break;
             }
             case 'it' : {
@@ -32,8 +55,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function applyInputMask(inputMask) {
-
-        const input = document.getElementById("phone");
         input.value = '';
         inputMask.mask(input);
     }
@@ -41,4 +62,48 @@ document.addEventListener('DOMContentLoaded', () => {
     // This is default value
     initInputMaskByLanguage(phoneSelect.value);
 
+    // form listeners
+
+    const button = document.getElementById('submitFormButton');
+    const throttledSubmitHandler = _.throttle(listenToFormSubmit, 1000);
+
+    function listenToFormSubmit() {
+        const inputValue = input.inputmask.unmaskedvalue();
+
+        console.log(toastr);
+        if (isNumberValid(inputValue)) {
+            submitFormData();
+        } else {
+            generateToastrTemplate('error', 'Пожалуйста, проверьте правильность номера');
+            input.focus();
+        }
+    }
+
+    function isNumberValid(number = '') {
+        return number.length >= 7;
+    }
+
+    function submitFormData() {
+
+        const formData = new FormData();
+
+        ['name', 'phone', 'message'].forEach(id => formData.append(id, document.getElementById(id).value));
+
+        axios.post('/formEndpoint', formData)
+
+            .then(serverResponse => {
+                //TODO check status
+                generateToastrTemplate('success', 'Форма успешно отправлена');
+            })
+            .catch(error => generateToastrTemplate('error', `На сервере произошла ошибка ${JSON.stringify(error)}`))
+    }
+
+    function generateToastrTemplate(method, message) {
+        toastr[method](`<p style="text-align: center;color: white;font-size: 20px">${message}</p>`);
+    }
+
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        throttledSubmitHandler();
+    });
 });
